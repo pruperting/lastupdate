38102d5b992f228d03c6684caa73f6c56c759a87
pruperting <r.plumridge@gmail.com>
2021-02-14 16:35:26 +0000

    Working version before I break it all

--- beetsplug/lastupdate.py
+++ beetsplug/lastupdate.py
@@ -1,29 +1,16 @@
 import subprocess
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
+from beets import config
 
 import sys
 import pylast
 
-API_KEY = "c3e3a25d7295d0e9b712c5ad9f5fa319"
-API_SECRET = "4417c92a23d24dfa2515540c3f69aad5"
-lastfm_username = "prupertplum"
-# You can use either use the password, or find the hash once and use that
-lastfm_password_hash = pylast.md5("nU65Kj8E2V9T!N2")
-
-lastfm_network = pylast.LastFMNetwork(
-    api_key=API_KEY,
-    api_secret=API_SECRET,
-    username=lastfm_username,
-    password_hash=lastfm_password_hash,
-)
-
-
-lastupdate_command = Subcommand('lsupdate', help='Update playtime based on recent lastfm activity')
-def last_update(lib, opts, args):
-    print "This part should update playcounts"
-    lastfm_upddate()
-lastupdate_command.func = last_update
+lastupdate_command = Subcommand('lastupdate', help='Update playtime based on recent lastfm activity')
+def lastupdate(lib, opts, args):
+    print ("This part should update playcounts")
+    lastfm_update()
+lastupdate_command.func = lastupdate
 
 class SuperPlug(BeetsPlugin):
     def commands(self):
@@ -31,7 +18,6 @@ class SuperPlug(BeetsPlugin):
 
 def track_and_timestamp(track):
     return f"{track.timestamp}\t{track.track}"
-
 
 def print_track(track):
     print(track_and_timestamp(track))
@@ -61,15 +47,19 @@ def split_artist_track(artist_track):
     return (artist, track)
 
 def get_recent_tracks(username, number):
+    lastfm_apikey = config['lastupdate']['apikey'].as_str()
+    lastfm_network = pylast.LastFMNetwork(
+    api_key=lastfm_apikey,
+    )
     recent_tracks = lastfm_network.get_user(username).get_recent_tracks(limit=number)
     for i, track in enumerate(recent_tracks):
         printable = track_and_timestamp(track)
         print(str(i + 1) + " " + printable)
     return recent_tracks
 
-def lastfm_upddate():
+def lastfm_update():
+    lastfm_username = config['lastupdate']['user'].as_str()
     print(lastfm_username + " last played:")
-    self._log.debug(u'Getting recently played tracks', item)
     try:
         get_recent_tracks(lastfm_username, 200)
     except pylast.WSError as e:
